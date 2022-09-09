bbadfd7d0afcb8577d23ecb1b788f09ef6ee6f36
jaimevlz <jaimevlz306@gmail.com>
2022-09-08 20:05:36 -0500

    Vite Class 1 V1

---
+++ react-task-vite/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  
+  return <h1>Hola Mundo!</h1>
+}
+
+export default App
+
+
+//  ul>li*4>a{enlace $} donde $ funciona como counter, * funciona como cant
+//
+{/* <ul>
+<li><a href="">Enlace 1</a></li>
+<li><a href="">Enlace 2</a></li>
+<li><a href="">Enlace 3</a></li>
+<li><a href="">Enlace 4</a></li>
+</ul> */}
